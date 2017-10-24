f08a3408e900dea85f21b6c80f77cec0481e00be
Vishnu B <vishnu@sourceeasy.com>
2017-10-24 16:05:16 +0530

    Removes duplicate code

--- feed/routes.py
+++ feed/routes.py
@@ -14,44 +14,5 @@ def index():
 		feed_url = request.form.get('url')
 		tags = request.form.getlist('tags')
 		return FeedController.store(feed_url, tags)
-		feed = feedparser.parse(feed_url)
 
-		if len(feed.entries) > 0:
-			# save to db
-			title = feed.feed.title
-			if 'icon' in feed.feed:
-				icon = feed.feed.icon
-			elif 'image' in feed.feed:
-				icon = feed.feed.image.href
-
-			newFeed = Feed(
-				title=title,
-				url='',
-				rss_url=feed_url,
-				icon=icon
-			)
-			db.session.add(newFeed)
-			db.session.commit()
-
-			
-
-			for entry in feed.entries:
-				title = entry.title
-				url = entry.link
-				content = entry.summary
-				author = entry.author if 'author' in entry else ''
-
-				newFeedArticle = FeedArticle(
-					title=title,
-					url=url,
-					content=content,
-					author=author,
-					feed_id=newFeed.id
-				)
-				db.session.add(newFeedArticle)
-				db.session.commit()
-
-		# return "Done"
-		return render_template('add_url_page.html', feed = feed)
-		
 	return render_template('add_url_page.html')
