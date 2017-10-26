e0d50042fd04e4a6e6d5dedf5fa33a7b2fa4460e
probir03 <31733739+probir03@users.noreply.github.com>
2017-10-26 20:34:32 +0530

    Delete env.py

--- env.py
+++
@@ -1,6 +0,0 @@
-APP_ENV = 'local'
-DEBUG = True
-SERVER_ACCESS_CODE = 'sezy.manage.secret.code'
-SECRET_KEY = 'sezysezy!!'
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/feedr'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
