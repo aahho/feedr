768d6c49ef25118b3d2bfaef18d1a59f51b80a52
Probir Gayen <probir@Probirs-MacBook-Pro.local>
2017-11-01 13:29:17 +0530

    templates updated

--- seeders/SeedManager.py
+++ seeders/SeedManager.py
@@ -7,8 +7,8 @@ import datetime
 def user_seeder():
     user_data = {
         'id': '1b3b4f40-2e7c-4a66-b1f1-0f19ff113369', 
-        'display_name':'AahoDev',
-        'email' : 'ahhodev@aaho.com',
+        'display_name':'AahhoDev',
+        'email' : 'aahhodev@aaho.com',
         'password' : helpers.hash_password('feedr123'),
         'created_at': datetime.datetime.now(),
         'is_god' : True,
@@ -19,7 +19,7 @@ def user_seeder():
     user_details = {
         'id' : 1,
         'user_id': '1b3b4f40-2e7c-4a66-b1f1-0f19ff113369', 
-        'first_name':'AahoDev',
+        'first_name':'AahhoDev',
         'last_name' : None,
         'created_at': datetime.datetime.now(),
         'updated_at': datetime.datetime.now(),
